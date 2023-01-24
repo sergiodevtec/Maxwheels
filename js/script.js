b4514b8580e0d3760a9d28eb954f0066a0291d1a
= <=>
2023-01-23 21:15:23 -0300

    Botão aciona modal login

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
 
-menu.onclick = () =>{
+
+  menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
   }
   
 
-  document.querySelector('#login-btn').onclick = () => {
+  document.querySelector('.login-btn').onclick = () => {
     document.querySelector('.login-form-container').classList.toggle('active');
   }
 
